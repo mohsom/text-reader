adfcb136c6c8275a28fb8a8ae2b8e6e505bb6bc0
mohsom <volodya.skalskyi@gmail.com>
2015-05-04 17:02:04 +0300

    Just a commit.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
 /**
  * Created by mohsom on 04.05.2015.
  */
-document.ready = function () {
-    function readFile(e) {
-        var f= e.target.files[0];
-        var reader = new FileReader();
-        reader.readAsText(f);
-        reader.onload =function(){
-            var text=reader.result;
-            var textElem=document.getElementById('book');
-            textElem.innerHTML='';
-            textElem.appendChild(document.createTextNode(text));
-        }
+function readFile(f) {
+    var reader = new FileReader();
+    reader.readAsText(f);
+    reader.onload = function () {
+        var text = reader.result;
+        var textElem = document.getElementById('book');
+        textElem.innerHTML = '';
+        textElem.appendChild(document.createTextNode(text));
     }
-    document.getElementById('file').addEventListener('change', readFile, false);
-};
+}
