f807500f4cb92599a74dfcd39a9752b198753f44
mohsom <volodya.skalskyi@gmail.com>
2015-05-04 16:18:27 +0300

    Just a commit.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 /**
  * Created by mohsom on 04.05.2015.
  */
-function readFile(e){
-    alert('dw');
-}
-document.getElementById('file').addEventListener('change', readFile, false);
+document.ready = function () {
+    function readFile(e) {
+        alert('dw');
+    }
+
+    document.getElementById('file').addEventListener('change', readFile, false);
+};
